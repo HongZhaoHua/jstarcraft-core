b2478936f209c997d75b52e895779aca05750729
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-03-29 13:23:19 +0800

    字符串工具支持各种标点符号.

--- src/main/java/com/jstarcraft/core/utility/StringUtility.java
+++ src/main/java/com/jstarcraft/core/utility/StringUtility.java
@@ -19,6 +19,51 @@ public class StringUtility extends StringUtils {
 	/** 字符串编码 */
 	public static final Charset CHARSET = Charset.forName(CharEncoding.UTF_8);
 
+	/** 星号 */
+	public static final String ASTERISK = "*";
+
+	/** 冒号 */
+	public static final String COLON = ":";
+
+	/** 逗号 */
+	public static final String COMMA = ",";
+
+	/** 破折号 */
+	public static final String DASH = "-";
+
+	/** 除号 */
+	public static final String DIVISION = "/";
+
+	/** 双引号 */
+	public static final String DOUBLE_QUOTATION = "\"";
+
+	/** 惊叹号 */
+	public static final String EXCLAMATION = "!";
+
+	/** 连字符 */
+	public static final String HYPHEN = "-";
+
+	/** 减号 */
+	public static final String MINUS = "-";
+
+	/** 乘号 */
+	public static final String MULTIPLICATION = "*";
+
+	/** 句号 */
+	public static final String PERIOD = ".";
+
+	/** 加号 */
+	public static final String PLUS = "+";
+
+	/** 问号 */
+	public static final String QUESTION = "?";
+
+	/** 分号 */
+	public static final String SEMICOLON = ";";
+
+	/** 单引号 */
+	public static final String SINGLE_QUOTATION = "'";
+
 	/**
 	 * 使用指定参数格式化指定模板,并转换为字符串
 	 * 
