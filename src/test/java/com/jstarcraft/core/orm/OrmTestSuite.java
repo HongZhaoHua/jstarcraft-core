05857b1e981b778d569861167942f0175e2bf481
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-03-30 14:36:11 +0800

    ORM模块集成测试

--- src/test/java/com/jstarcraft/core/orm/OrmTestSuite.java
+++ src/test/java/com/jstarcraft/core/orm/OrmTestSuite.java
@@ -7,9 +7,10 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.jstarcraft.core.orm.berkeley.BerkeleyTestSuite;
 import com.jstarcraft.core.orm.hibernate.HibernateTestSuite;
 import com.jstarcraft.core.orm.mongo.MongoTestSuite;
+import com.jstarcraft.core.orm.mybatis.MyBatisAccessorTestCase;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BerkeleyTestSuite.class, HibernateTestSuite.class, MongoTestSuite.class })
+@SuiteClasses({ BerkeleyTestSuite.class, HibernateTestSuite.class, MongoTestSuite.class, MyBatisAccessorTestCase.class })
 public class OrmTestSuite {
 
 }
