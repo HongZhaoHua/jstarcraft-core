430054fc0f783aad274768c2ee4a6ff451260a9a
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-05-08 16:42:19 +0800

    StringUtility支持百分号.

--- jstarcraft-core-common/src/main/java/com/jstarcraft/core/utility/StringUtility.java
+++ jstarcraft-core-common/src/main/java/com/jstarcraft/core/utility/StringUtility.java
@@ -16,86 +16,89 @@ import org.slf4j.helpers.MessageFormatter;
  */
 public class StringUtility extends StringUtils {
 
-	/** 字符串编码 */
-	public static final Charset CHARSET = Charset.forName(CharEncoding.UTF_8);
+    /** 字符串编码 */
+    public static final Charset CHARSET = Charset.forName(CharEncoding.UTF_8);
 
-	/** 星号 */
-	public static final String ASTERISK = "*";
+    /** 星号 */
+    public static final String ASTERISK = "*";
 
-	/** 反斜杆 */
-	public static final String BACKWARD_SLASH = "\\";
+    /** 反斜杆 */
+    public static final String BACKWARD_SLASH = "\\";
 
-	/** 冒号 */
-	public static final String COLON = ":";
+    /** 冒号 */
+    public static final String COLON = ":";
 
-	/** 逗号 */
-	public static final String COMMA = ",";
+    /** 逗号 */
+    public static final String COMMA = ",";
 
-	/** 破折号 */
-	public static final String DASH = "-";
+    /** 破折号 */
+    public static final String DASH = "-";
 
-	/** 除号 */
-	public static final String DIVISION = "/";
+    /** 除号 */
+    public static final String DIVISION = "/";
 
-	/** 双引号 */
-	public static final String DOUBLE_QUOTATION = "\"";
+    /** 双引号 */
+    public static final String DOUBLE_QUOTATION = "\"";
 
-	/** 惊叹号 */
-	public static final String EXCLAMATION = "!";
+    /** 惊叹号 */
+    public static final String EXCLAMATION = "!";
 
-	/** 正斜杆 */
-	public static final String FORWARD_SLASH = "/";
+    /** 正斜杆 */
+    public static final String FORWARD_SLASH = "/";
 
-	/** 井号 */
-	public static final String HASH = "#";
+    /** 井号 */
+    public static final String HASH = "#";
 
-	/** 连字符 */
-	public static final String HYPHEN = "-";
+    /** 连字符 */
+    public static final String HYPHEN = "-";
 
-	/** 减号 */
-	public static final String MINUS = "-";
+    /** 减号 */
+    public static final String MINUS = "-";
 
-	/** 乘号 */
-	public static final String MULTIPLICATION = "*";
+    /** 乘号 */
+    public static final String MULTIPLICATION = "*";
 
-	/** 句号 */
-	public static final String PERIOD = ".";
+    /** 句号 */
+    public static final String PERIOD = ".";
 
-	/** 加号 */
-	public static final String PLUS = "+";
+    /** 百分号 */
+    public static final String PERCENT = "%";
 
-	/** 问号 */
-	public static final String QUESTION = "?";
+    /** 加号 */
+    public static final String PLUS = "+";
 
-	/** 分号 */
-	public static final String SEMICOLON = ";";
+    /** 问号 */
+    public static final String QUESTION = "?";
 
-	/** 单引号 */
-	public static final String SINGLE_QUOTATION = "'";
+    /** 分号 */
+    public static final String SEMICOLON = ";";
 
-	/** 下划线 */
-	public static final String UNDERSCORE = "_";
+    /** 单引号 */
+    public static final String SINGLE_QUOTATION = "'";
 
-	/**
-	 * 使用指定参数格式化指定模板,并转换为字符串
-	 * 
-	 * @param template
-	 * @param parameters
-	 * @return
-	 */
-	public static final String format(String template, Object... parameters) {
-		FormattingTuple formatter = MessageFormatter.arrayFormat(template, parameters);
-		return formatter.getMessage();
-	}
+    /** 下划线 */
+    public static final String UNDERSCORE = "_";
 
-	/**
-	 * 将指定对象通过反射转换为字符串
-	 * 
-	 * @param object
-	 * @return
-	 */
-	public static final String reflect(Object object) {
-		return ReflectionToStringBuilder.toString(object);
-	}
+    /**
+     * 使用指定参数格式化指定模板,并转换为字符串
+     * 
+     * @param template
+     * @param parameters
+     * @return
+     */
+    public static final String format(String template, Object... parameters) {
+        FormattingTuple formatter = MessageFormatter.arrayFormat(template, parameters);
+        return formatter.getMessage();
+    }
+
+    /**
+     * 将指定对象通过反射转换为字符串
+     * 
+     * @param object
+     * @return
+     */
+    public static final String reflect(Object object) {
+        return ReflectionToStringBuilder.toString(object);
+    }
 
 }
