44d1fa5c8ef2c5c6237ced4a0a2349ebd9e8e4a1
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-08-12 15:00:07 +0800

    PackageUtility不处理匿名类.

--- jstarcraft-core-common/src/main/java/com/jstarcraft/core/utility/PackageUtility.java
+++ jstarcraft-core-common/src/main/java/com/jstarcraft/core/utility/PackageUtility.java
@@ -27,118 +27,126 @@ import org.slf4j.LoggerFactory;
  */
 public class PackageUtility {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(PackageUtility.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PackageUtility.class);
 
-	public static class PackageScanner {
-		private final Collection<Class<?>> clazzCollection = new HashSet<Class<?>>();
+    public static class PackageScanner {
+        private final Collection<Class<?>> clazzCollection = new HashSet<Class<?>>();
 
-		public PackageScanner(final String... packageNames) {
-			for (String packageName : packageNames) {
-				try {
-					final String packageDirectory = packageName.replace('.', '/');
-					final Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(packageDirectory);
-					while (urls.hasMoreElements()) {
-						final URL url = urls.nextElement();
-						if ("file".equals(url.getProtocol())) {
-							final File directory = new File(url.getPath());
-							if (!directory.isDirectory()) {
-								throw new RuntimeException("package:[" + packageName + "] is not directory");
-							}
-							clazzCollection.addAll(PackageUtility.scanClassByDirectory(packageName, directory));
-						} else if ("jar".equals(url.getProtocol())) {
-							final JarFile jar = ((JarURLConnection) url.openConnection()).getJarFile();
-							clazzCollection.addAll(PackageUtility.scanClassByJar(packageName, jar));
-						}
-					}
-				} catch (IOException exception) {
-					throw new RuntimeException(exception);
-				}
-			}
-		}
+        public PackageScanner(final String... packageNames) {
+            for (String packageName : packageNames) {
+                try {
+                    final String packageDirectory = packageName.replace('.', '/');
+                    final Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(packageDirectory);
+                    while (urls.hasMoreElements()) {
+                        final URL url = urls.nextElement();
+                        if ("file".equals(url.getProtocol())) {
+                            final File directory = new File(url.getPath());
+                            if (!directory.isDirectory()) {
+                                throw new RuntimeException("package:[" + packageName + "] is not directory");
+                            }
+                            clazzCollection.addAll(PackageUtility.scanClassByDirectory(packageName, directory));
+                        } else if ("jar".equals(url.getProtocol())) {
+                            final JarFile jar = ((JarURLConnection) url.openConnection()).getJarFile();
+                            clazzCollection.addAll(PackageUtility.scanClassByJar(packageName, jar));
+                        }
+                    }
+                } catch (IOException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+        }
 
-		public PackageScanner(final Collection<String> packageNames) {
-			this(packageNames.toArray(new String[packageNames.size()]));
-		}
+        public PackageScanner(final Collection<String> packageNames) {
+            this(packageNames.toArray(new String[packageNames.size()]));
+        }
 
-		public Collection<Class<?>> getClazzCollection() {
-			return this.clazzCollection;
-		}
-	}
+        public Collection<Class<?>> getClazzCollection() {
+            return this.clazzCollection;
+        }
+    }
 
-	/**
-	 * 按照Jar扫描类
-	 * 
-	 * @param packageName
-	 * @param jar
-	 * @return
-	 */
-	public static Collection<Class<?>> scanClassByJar(final String packageName, final JarFile jar) {
-		final Enumeration<JarEntry> jarEntries = jar.entries();
-		final Pattern pattern = Pattern.compile("(" + packageName.replace('.', '/') + ".*)\\.class");
-		final Collection<Class<?>> clazzCollection = new HashSet<Class<?>>();
+    /**
+     * 按照Jar扫描类
+     * 
+     * @param packageName
+     * @param jar
+     * @return
+     */
+    public static Collection<Class<?>> scanClassByJar(final String packageName, final JarFile jar) {
+        final Enumeration<JarEntry> jarEntries = jar.entries();
+        final Pattern pattern = Pattern.compile("(" + packageName.replace('.', '/') + ".*)\\.class");
+        final Collection<Class<?>> clazzCollection = new HashSet<Class<?>>();
 
-		while (jarEntries.hasMoreElements()) {
-			final JarEntry entry = jarEntries.nextElement();
-			final String name = entry.getName();
-			final Matcher matcher = pattern.matcher(name.replace(File.separatorChar, '/'));
-			if (matcher.find()) {
-				final String className = matcher.group(1).replace('/', '.');
-				try {
-					final Class<?> clazz = Class.forName(className);
-					clazzCollection.add(clazz);
-				} catch (ClassNotFoundException exception) {
-					LOGGER.error("无法加载类[{}]", className, exception);
-				}
-			}
-		}
-		return clazzCollection;
-	}
+        while (jarEntries.hasMoreElements()) {
+            final JarEntry entry = jarEntries.nextElement();
+            final String name = entry.getName();
+            final Matcher matcher = pattern.matcher(name.replace(File.separatorChar, '/'));
+            if (matcher.find()) {
+                final String className = matcher.group(1).replace('/', '.');
+                // 处理匿名类
+                if (className.contains("$")) {
+                    continue;
+                }
+                try {
+                    final Class<?> clazz = Class.forName(className);
+                    clazzCollection.add(clazz);
+                } catch (ClassNotFoundException exception) {
+                    LOGGER.error("无法加载类[{}]", className, exception);
+                }
+            }
+        }
+        return clazzCollection;
+    }
 
-	/**
-	 * 按照目录扫描类
-	 * 
-	 * @param packageName
-	 * @param directory
-	 * @return
-	 */
-	public static Collection<Class<?>> scanClassByDirectory(final String packageName, final File directory) {
-		final Stack<File> scanDirectories = new Stack<File>();
-		final Collection<File> classFiles = new ArrayList<File>();
-		final FileFilter fileFilter = new FileFilter() {
+    /**
+     * 按照目录扫描类
+     * 
+     * @param packageName
+     * @param directory
+     * @return
+     */
+    public static Collection<Class<?>> scanClassByDirectory(final String packageName, final File directory) {
+        final Stack<File> scanDirectories = new Stack<File>();
+        final Collection<File> classFiles = new ArrayList<File>();
+        final FileFilter fileFilter = new FileFilter() {
 
-			@Override
-			public boolean accept(File file) {
-				if (file.isDirectory()) {
-					scanDirectories.push(file);
-					return false;
-				}
-				return file.getName().matches(".*\\.class$");
-			}
+            @Override
+            public boolean accept(File file) {
+                if (file.isDirectory()) {
+                    scanDirectories.push(file);
+                    return false;
+                }
+                return file.getName().matches(".*\\.class$");
+            }
 
-		};
+        };
 
-		scanDirectories.push(directory);
+        scanDirectories.push(directory);
 
-		while (!scanDirectories.isEmpty()) {
-			final File scanDirectory = scanDirectories.pop();
-			Collections.addAll(classFiles, scanDirectory.listFiles(fileFilter));
-		}
-		final Pattern pattern = Pattern.compile("(" + packageName.replace('.', '/') + ".*)\\.class");
-		final Collection<Class<?>> clazzCollection = new HashSet<Class<?>>();
-		for (File file : classFiles) {
-			final Matcher matcher = pattern.matcher(file.getAbsolutePath().replace(File.separatorChar, '/'));
-			if (matcher.find()) {
-				final String className = matcher.group(1).replace('/', '.');
-				try {
-					final Class<?> clazz = Class.forName(className);
-					clazzCollection.add(clazz);
-				} catch (ClassNotFoundException exception) {
-					LOGGER.error("无法加载类[{}]", className, exception);
-				}
-			}
-		}
+        while (!scanDirectories.isEmpty()) {
+            final File scanDirectory = scanDirectories.pop();
+            Collections.addAll(classFiles, scanDirectory.listFiles(fileFilter));
+        }
+        final Pattern pattern = Pattern.compile("(" + packageName.replace('.', '/') + ".*)\\.class");
+        final Collection<Class<?>> clazzCollection = new HashSet<Class<?>>();
+        for (File file : classFiles) {
+            final Matcher matcher = pattern.matcher(file.getAbsolutePath().replace(File.separatorChar, '/'));
+            if (matcher.find()) {
+                final String className = matcher.group(1).replace('/', '.');
+                // 处理匿名类
+                if (className.contains("$")) {
+                    continue;
+                }
+                try {
+                    final Class<?> clazz = Class.forName(className);
+                    clazzCollection.add(clazz);
+                } catch (ClassNotFoundException exception) {
+                    LOGGER.error("无法加载类[{}]", className, exception);
+                }
+            }
+        }
 
-		return clazzCollection;
-	}
+        return clazzCollection;
+    }
 
 }
