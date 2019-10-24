77afa37685c2fcf37ba1833a17169d12489d3740
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-10-24 11:49:22 +0800

    Knn重构为Neighborhood.

---
+++ jstarcraft-core-common/src/main/java/com/jstarcraft/core/utility/Neighborhood.java
@@ -0,0 +1,63 @@
+package com.jstarcraft.core.utility;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+/**
+ * 附近
+ * 
+ * @author Birdy
+ *
+ * @param <T>
+ */
+public class Neighborhood<T> {
+
+    private int k;
+
+    private Comparator<T> comparator;
+
+    private NavigableSet<T> neighbors;
+
+    private volatile T keyValue;
+
+    public Neighborhood(int k, Comparator<T> comparator) {
+        this.k = k;
+        this.comparator = comparator;
+        this.neighbors = new TreeSet<>(comparator);
+    }
+
+    /**
+     * 更新邻居
+     * 
+     * @param key
+     * @param value
+     */
+    public synchronized void updateNeighbor(T keyValue) {
+        if (neighbors.size() >= k) {
+            // 与边界值比较再判断是否更新
+            if (comparator.compare(keyValue, this.keyValue) < 0) {
+                neighbors.add(keyValue);
+                if (neighbors.size() > k) {
+                    // 防止大小不变的情况下
+                    neighbors.pollLast();
+                }
+                this.keyValue = neighbors.last();
+            }
+        } else {
+            neighbors.add(keyValue);
+            this.keyValue = neighbors.last();
+        }
+    }
+
+    /**
+     * 获取邻居
+     * 
+     * @return
+     */
+    public Collection<T> getNeighbors() {
+        return neighbors;
+    }
+
+}
