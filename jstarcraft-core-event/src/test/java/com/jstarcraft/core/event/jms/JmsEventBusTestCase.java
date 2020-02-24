1c4476c2fcff5a83086e066a7e393a1710ae58c6
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-02-24 08:55:56 +0800

    清理JMS测试消息.

--- jstarcraft-core-event/src/test/java/com/jstarcraft/core/event/jms/JmsEventBusTestCase.java
+++ jstarcraft-core-event/src/test/java/com/jstarcraft/core/event/jms/JmsEventBusTestCase.java
@@ -1,5 +1,9 @@
 package com.jstarcraft.core.event.jms;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.jms.Destination;
+import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
@@ -13,6 +17,7 @@ import com.jstarcraft.core.event.EventBus;
 import com.jstarcraft.core.event.EventBusTestCase;
 import com.jstarcraft.core.event.EventMode;
 import com.jstarcraft.core.event.MockEvent;
+import com.jstarcraft.core.utility.StringUtility;
 
 public class JmsEventBusTestCase extends EventBusTestCase {
 
@@ -24,7 +29,19 @@ public class JmsEventBusTestCase extends EventBusTestCase {
     }
 
     @AfterClass
-    public static void stop() {
+    public static void stop() throws Exception {
+        JMSContext context = factory.createContext();
+        Destination destination = context.createQueue(MockEvent.class.getName());
+        JMSConsumer consumer = context.createConsumer(destination);
+        // 清理测试消息
+        logger.info("清理JMS测试消息开始");
+        AtomicInteger count = new AtomicInteger();
+        consumer.setMessageListener((data) -> {
+            String message = StringUtility.format("清理JMS测试消息[{}]", count.incrementAndGet());
+            logger.info(message);
+        });
+        Thread.sleep(1000L);
+        logger.info("清理JMS测试消息结束");
         factory.close();
     }
 
