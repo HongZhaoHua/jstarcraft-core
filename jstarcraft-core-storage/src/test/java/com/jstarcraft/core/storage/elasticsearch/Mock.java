0a50c75be2974c834cf3ea739c8e4944368a89c1
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-05-13 16:33:53 +0800

    JStarCraft Core Storage模块准备支持Elasticsearch.

---
+++ jstarcraft-core-storage/src/test/java/com/jstarcraft/core/storage/elasticsearch/Mock.java
@@ -0,0 +1,71 @@
+package com.jstarcraft.core.storage.elasticsearch;
+
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "item", type = "Item")
+public class Mock {
+
+    @Id
+    private Long id;
+
+    /** 标题 */
+    @Field(type = FieldType.Text)
+    private String title;
+
+    /** 类目 */
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    /** 价格 */
+    @Field(type = FieldType.Double)
+    private Double price;
+
+    Mock() {
+    }
+
+    public Mock(Long id, String title, String category, Double price) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(category, id, price, title);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null)
+            return false;
+        if (getClass() != object.getClass())
+            return false;
+        Mock that = (Mock) object;
+        return Objects.equals(this.category, that.category) && Objects.equals(this.id, that.id) && Objects.equals(this.price, that.price) && Objects.equals(this.title, that.title);
+    }
+
+}
