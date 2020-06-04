a7ff27cbabbb56bda122a50251efc01ec6f0ea4b
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-06-04 19:37:38 +0800

    嵌入式Kafka的Maven配置.

--- jstarcraft-core-event/src/test/java/com/jstarcraft/core/event/kafka/KafkaTestCase.java
+++ jstarcraft-core-event/src/test/java/com/jstarcraft/core/event/kafka/KafkaTestCase.java
@@ -13,9 +13,10 @@ public class KafkaTestCase {
         Properties properties = new Properties();
         properties.put("broker.id", "1");
         properties.put("port", "9092");
+        // 注意Maven的配置
+        // https://github.com/salesforce/kafka-junit/tree/master/kafka-junit4
         KafkaTestServer server = new KafkaTestServer(properties);
         server.start();
-        String string = server.getKafkaBrokers().getBrokerById(1).getConnectString();
         server.stop();
     }
 
