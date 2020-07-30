9f9583ddcd7f81954b8f4c171d623c9c3c130e23
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-07-30 19:55:44 +0800

    修复SnackJsonSelector选取指定元素为非数组的Bug.

--- jstarcraft-core-common/src/main/java/com/jstarcraft/core/common/selection/jsonpath/SnackJsonPathSelector.java
+++ jstarcraft-core-common/src/main/java/com/jstarcraft/core/common/selection/jsonpath/SnackJsonPathSelector.java
@@ -1,5 +1,6 @@
 package com.jstarcraft.core.common.selection.jsonpath;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.noear.snack.ONode;
@@ -14,7 +15,8 @@ public class SnackJsonPathSelector extends JsonPathSelector<ONode> {
 
     @Override
     public Collection<ONode> selectContent(ONode content) {
-        return content.select(query).ary();
+        content = content.select(query);
+        return content.isArray() ? content.ary() : Arrays.asList(content);
     }
 
 }
