8b0a44481bb1f6f11ad4d1920d3385a3d826f506
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-09-09 18:13:48 +0800

    重构单元测试.

--- jstarcraft-core-codec/src/test/java/com/jstarcraft/core/codec/CodecTestSuite.java
+++ jstarcraft-core-codec/src/test/java/com/jstarcraft/core/codec/CodecTestSuite.java
@@ -7,10 +7,26 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.jstarcraft.core.codec.csv.CsvContentCodecTestCase;
 import com.jstarcraft.core.codec.json.JsonContentCodecTestCase;
 import com.jstarcraft.core.codec.kryo.KryoContentCodecTestCase;
+import com.jstarcraft.core.codec.msgpack.MessagePackContentCodecTestCase;
 import com.jstarcraft.core.codec.protocolbufferx.ProtocolBufferXContentCodecTestCase;
+import com.jstarcraft.core.codec.yaml.YamlContentCodecTestCase;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CodecDefinitionTestCase.class, CsvContentCodecTestCase.class, JsonContentCodecTestCase.class, KryoContentCodecTestCase.class, ProtocolBufferXContentCodecTestCase.class })
+@SuiteClasses({
+
+        CodecDefinitionTestCase.class,
+
+        CsvContentCodecTestCase.class,
+
+        JsonContentCodecTestCase.class,
+
+        KryoContentCodecTestCase.class,
+
+        MessagePackContentCodecTestCase.class,
+
+        ProtocolBufferXContentCodecTestCase.class,
+
+        YamlContentCodecTestCase.class })
 public class CodecTestSuite {
 
 }
