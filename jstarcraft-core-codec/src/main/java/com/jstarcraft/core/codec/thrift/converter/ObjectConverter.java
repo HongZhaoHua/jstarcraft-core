5184760002eb8161cbb5ffa7f101093e1a871937
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-09-24 18:50:39 +0800

    重构ObjectConverter,不使用标记null.

--- jstarcraft-core-codec/src/main/java/com/jstarcraft/core/codec/thrift/converter/ObjectConverter.java
+++ jstarcraft-core-codec/src/main/java/com/jstarcraft/core/codec/thrift/converter/ObjectConverter.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Type;
 import org.apache.thrift.protocol.TField;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.protocol.TStruct;
+import org.apache.thrift.protocol.TType;
 
 import com.jstarcraft.core.codec.exception.CodecConvertionException;
 import com.jstarcraft.core.codec.specification.ClassDefinition;
@@ -19,85 +20,72 @@ import com.jstarcraft.core.utility.StringUtility;
  */
 public class ObjectConverter extends ThriftConverter<Object> {
 
-    /**
-     * 空标记
-     */
-    private static final byte NULL = 1;
-    /**
-     * 非空标记
-     */
-    private static final byte NOT_NULL = 0;
-
     @Override
     public Object readValueFrom(ThriftContext context, Type type, ClassDefinition definition) throws Exception {
         TProtocol protocol = context.getProtocol();
-        byte nil = protocol.readByte();
         protocol.readStructBegin();
-        PropertyDefinition[] properties = definition.getProperties();
-        Object object;
-        try {
-            object = definition.getInstance();
-        } catch (Exception exception) {
-            String message = StringUtility.format("获取类型[{}]实例异常", definition.getName());
-            throw new CodecConvertionException(message, exception);
-        }
-        for (int index = 0; index < properties.length; index++) {
-            PropertyDefinition property = properties[index];
-            protocol.readFieldBegin();
-            ThriftConverter converter = context.getProtocolConverter(property.getSpecification());
-            definition = context.getClassDefinition(property.getCode());
-            Object value = converter.readValueFrom(context, property.getType(), definition);
+        protocol.readFieldBegin();
+        boolean nullable = protocol.readBool();
+        protocol.readFieldEnd();
+        Object object = null;
+        if (!nullable) {
             try {
-                property.setValue(object, value);
+                object = definition.getInstance();
             } catch (Exception exception) {
-                String message = StringUtility.format("赋值[{}]实例属性[{}]异常", definition.getName(), property.getName());
+                String message = StringUtility.format("获取类型[{}]实例异常", definition.getName());
                 throw new CodecConvertionException(message, exception);
             }
-            protocol.readFieldEnd();
+            PropertyDefinition[] properties = definition.getProperties();
+            while (true) {
+                TField field = protocol.readFieldBegin();
+                if (field.type == org.apache.thrift.protocol.TType.STOP) {
+                    break;
+                }
+                PropertyDefinition property = properties[field.id - 2];
+                ThriftConverter converter = context.getProtocolConverter(property.getSpecification());
+                definition = context.getClassDefinition(property.getCode());
+                Object value = converter.readValueFrom(context, property.getType(), definition);
+                try {
+                    property.setValue(object, value);
+                } catch (Exception exception) {
+                    String message = StringUtility.format("赋值[{}]实例属性[{}]异常", definition.getName(), property.getName());
+                    throw new CodecConvertionException(message, exception);
+                }
+                protocol.readFieldEnd();
+            }
         }
-        // 读TType.STOP
-        protocol.readFieldBegin();
         protocol.readStructEnd();
-        if (nil == NULL) {
-            return null;
-        }
         return object;
     }
 
     @Override
     public void writeValueTo(ThriftContext context, Type type, ClassDefinition definition, Object value) throws Exception {
         TProtocol protocol = context.getProtocol();
-        if (value == null) {
-            protocol.writeByte(NULL);
-        } else {
-            protocol.writeByte(NOT_NULL);
-        }
+        boolean nullable = value == null;
         protocol.writeStructBegin(new TStruct(definition.getName()));
-        PropertyDefinition[] properties = definition.getProperties();
-        if (value == null) {
-            try {
-                value = definition.getInstance();
-            } catch (Exception exception) {
-                String message = StringUtility.format("获取类型[{}]实例异常", definition.getName());
-                throw new CodecConvertionException(message, exception);
+        protocol.writeFieldBegin(new TField("_nullable", TType.BOOL, (short) (1)));
+        protocol.writeBool(nullable);
+        protocol.writeFieldEnd();
+        if (!nullable) {
+            PropertyDefinition[] properties = definition.getProperties();
+            for (int index = 0; index < properties.length; index++) {
+                PropertyDefinition property = properties[index];
+                Object object;
+                try {
+                    object = property.getValue(value);
+                    if (object != null) {
+                        protocol.writeFieldBegin(new TField(property.getName(), context.getThriftType(property.getType()), (short) (index + 2)));
+                        ThriftConverter converter = context.getProtocolConverter(property.getSpecification());
+                        definition = context.getClassDefinition(property.getCode());
+                        converter.writeValueTo(context, property.getType(), definition, object);
+                        protocol.writeFieldEnd();
+                    }
+                } catch (Exception exception) {
+                    String message = StringUtility.format("取值[{}]实例属性[{}]异常", definition.getName(), property.getName());
+                    throw new CodecConvertionException(message, exception);
+                }
             }
         }
-        for (int index = 0; index < properties.length; index++) {
-            PropertyDefinition property = properties[index];
-            protocol.writeFieldBegin(new TField(property.getName(), context.getThriftType(property.getType()), (short) (index + 1)));
-            Object object;
-            try {
-                ThriftConverter converter = context.getProtocolConverter(property.getSpecification());
-                definition = context.getClassDefinition(property.getCode());
-                object = property.getValue(value);
-                converter.writeValueTo(context, property.getType(), definition, object);
-            } catch (Exception exception) {
-                String message = StringUtility.format("取值[{}]实例属性[{}]异常", definition.getName(), property.getName());
-                throw new CodecConvertionException(message, exception);
-            }
-            protocol.writeFieldEnd();
-        }
-        // 写TType.STOP
         protocol.writeFieldStop();
         protocol.writeStructEnd();
     }
