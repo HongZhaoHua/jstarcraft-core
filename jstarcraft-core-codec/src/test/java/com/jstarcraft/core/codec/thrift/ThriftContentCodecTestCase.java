c57acc5a80ad57bdd866c0f650f01622b453dc15
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-09-24 11:31:04 +0800

    格式化代码.

--- jstarcraft-core-codec/src/test/java/com/jstarcraft/core/codec/thrift/ThriftContentCodecTestCase.java
+++ jstarcraft-core-codec/src/test/java/com/jstarcraft/core/codec/thrift/ThriftContentCodecTestCase.java
@@ -2,16 +2,8 @@ package com.jstarcraft.core.codec.thrift;
 
 import com.jstarcraft.core.codec.ContentCodec;
 import com.jstarcraft.core.codec.ContentCodecTestCase;
-import com.jstarcraft.core.codec.json.JsonContentCodec;
 import com.jstarcraft.core.codec.specification.CodecDefinition;
 
-import java.lang.reflect.Type;
-
-/**
- * @author huang hong fei
- * @createAt 2020/9/23
- * @description
- */
 public class ThriftContentCodecTestCase extends ContentCodecTestCase {
 
     @Override
@@ -20,58 +12,4 @@ public class ThriftContentCodecTestCase extends ContentCodecTestCase {
         return codec;
     }
 
-    @Override
-    public void testArray() throws Exception {
-        super.testArray();
-    }
-
-    @Override
-    public void testBoolean() throws Exception {
-        super.testBoolean();
-    }
-
-    @Override
-    public void testInstant() throws Exception {
-        super.testInstant();
-    }
-
-    @Override
-    public void testComplex() throws Exception {
-        super.testComplex();
-    }
-
-    @Override
-    public void testNumber() throws Exception {
-        super.testNumber();
-    }
-
-    @Override
-    public void testNull() throws Exception {
-        super.testNull();
-    }
-
-    @Override
-    public void testString() throws Exception {
-        super.testString();
-    }
-
-    @Override
-    public void testType() throws Exception {
-        super.testType();
-    }
-
-    @Override
-    public void testUniMi() throws Exception {
-        super.testUniMi();
-    }
-
-    @Override
-    public void testPerformance() {
-        super.testPerformance();
-    }
-
-    @Override
-    protected void testConvert(Type type, Object value) throws Exception {
-        super.testConvert(type, value);
-    }
 }
