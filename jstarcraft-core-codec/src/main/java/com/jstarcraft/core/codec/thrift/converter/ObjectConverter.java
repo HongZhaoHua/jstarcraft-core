10281e881d3ea8608c33c0e7bbc2910e84c7d3e1
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-09-27 18:48:10 +0800

    重构Thrift协议的ObjectConverter.

--- jstarcraft-core-codec/src/main/java/com/jstarcraft/core/codec/thrift/converter/ObjectConverter.java
+++ jstarcraft-core-codec/src/main/java/com/jstarcraft/core/codec/thrift/converter/ObjectConverter.java
@@ -20,67 +20,68 @@ import com.jstarcraft.core.utility.StringUtility;
  */
 public class ObjectConverter extends ThriftConverter<Object> {
 
+    protected static final TField NULL_MARK = new TField(StringUtility.EMPTY, TType.BYTE, (short) 1);
+    
     @Override
     public Object readValueFrom(ThriftContext context, Type type, ClassDefinition definition) throws Exception {
         TProtocol protocol = context.getProtocol();
         protocol.readStructBegin();
-        protocol.readFieldBegin();
-        boolean mark = protocol.readBool();
-        protocol.readFieldEnd();
-        Object object = null;
-        if (!mark) {
+        TField feild = protocol.readFieldBegin();
+        Object instance;
+        if (feild.id == 1) {
+            instance = null;
+        } else {
             try {
-                object = definition.getInstance();
+                instance = definition.getInstance();
             } catch (Exception exception) {
                 String message = StringUtility.format("获取类型[{}]实例异常", definition.getName());
                 throw new CodecConvertionException(message, exception);
             }
-            PropertyDefinition[] properties = definition.getProperties();
-            while (true) {
-                TField field = protocol.readFieldBegin();
-                if (field.type == TType.STOP) {
-                    break;
-                }
-                PropertyDefinition property = properties[field.id - 2];
+        }
+        PropertyDefinition[] properties = definition.getProperties();
+        while (true) {
+            if (feild.type == TType.STOP) {
+                break;
+            }
+            if (feild.id != 1) {
+                PropertyDefinition property = properties[feild.id - 2];
                 ThriftConverter converter = context.getProtocolConverter(property.getSpecification());
                 definition = context.getClassDefinition(property.getCode());
                 Object value = converter.readValueFrom(context, property.getType(), definition);
                 try {
-                    property.setValue(object, value);
+                    property.setValue(instance, value);
                 } catch (Exception exception) {
                     String message = StringUtility.format("赋值[{}]实例属性[{}]异常", definition.getName(), property.getName());
                     throw new CodecConvertionException(message, exception);
                 }
-                protocol.readFieldEnd();
             }
-        } else {
-            protocol.readFieldBegin();
+            protocol.readFieldEnd();
+            feild = protocol.readFieldBegin();
         }
         protocol.readStructEnd();
-        return object;
+        return instance;
     }
 
     @Override
-    public void writeValueTo(ThriftContext context, Type type, ClassDefinition definition, Object value) throws Exception {
+    public void writeValueTo(ThriftContext context, Type type, ClassDefinition definition, Object instance) throws Exception {
         TProtocol protocol = context.getProtocol();
-        boolean mark = value == null;
         protocol.writeStructBegin(new TStruct(definition.getName()));
-        protocol.writeFieldBegin(new TField("_mark", TType.BOOL, (short) (1)));
-        protocol.writeBool(mark);
-        protocol.writeFieldEnd();
-        if (!mark) {
+        if (instance == null) {
+            protocol.writeFieldBegin(NULL_MARK);
+            protocol.writeFieldEnd();
+        } else {
             PropertyDefinition[] properties = definition.getProperties();
             for (int index = 0; index < properties.length; index++) {
                 PropertyDefinition property = properties[index];
-                Object object;
+                Object value;
                 try {
-                    object = property.getValue(value);
-                    if (object != null) {
+                    value = property.getValue(instance);
+                    if (value != null) {
                         TField field = new TField(property.getName(), context.getThriftType(property.getType()), (short) (index + 2));
                         protocol.writeFieldBegin(field);
                         ThriftConverter converter = context.getProtocolConverter(property.getSpecification());
                         definition = context.getClassDefinition(property.getCode());
-                        converter.writeValueTo(context, property.getType(), definition, object);
+                        converter.writeValueTo(context, property.getType(), definition, value);
                         protocol.writeFieldEnd();
                     }
                 } catch (Exception exception) {
