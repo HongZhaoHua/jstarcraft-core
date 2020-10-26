f0389fae21b5c828aa14489efbc67e4e14ff1aad
C5313739 <zhenwei.yue@sap.com>
2020-10-26 17:00:13 +0800

    ion:   1.增加Ion类型编码   2.增加UT

---
+++ jstarcraft-core-codec/src/test/java/com/jstarcraft/core/codec/ion/IonContentCodecTestCase.java
@@ -0,0 +1,15 @@
+package com.jstarcraft.core.codec.ion;
+
+import com.jstarcraft.core.codec.ContentCodec;
+import com.jstarcraft.core.codec.ContentCodecTestCase;
+import com.jstarcraft.core.codec.cbor.CborContentCodec;
+import com.jstarcraft.core.codec.specification.CodecDefinition;
+
+public class IonContentCodecTestCase extends ContentCodecTestCase {
+
+    @Override
+    protected ContentCodec getContentCodec(CodecDefinition protocolDefinition) {
+        return new IonContentCodec();
+    }
+
+}
