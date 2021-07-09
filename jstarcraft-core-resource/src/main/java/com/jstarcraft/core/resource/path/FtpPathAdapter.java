5be8159cbe1691e1667f40e044a153a1812e8c94
Birdy <Birdy@192.168.2.60>
2021-07-09 10:24:01 +0800

    准备支持JStarCraft Core IO模块.

---
+++ jstarcraft-core-resource/src/main/java/com/jstarcraft/core/resource/path/FtpPathAdapter.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.core.resource.path;
+
+import java.io.InputStream;
+
+public class FtpPathAdapter implements PathAdapter {
+
+    @Override
+    public InputStream getStream(String path) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ jstarcraft-core-resource/src/main/java/com/jstarcraft/core/resource/path/SvnPathAdapter.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.core.resource.path;
+
+import java.io.InputStream;
+
+public class SvnPathAdapter implements PathAdapter {
+
+    @Override
+    public InputStream getStream(String path) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
