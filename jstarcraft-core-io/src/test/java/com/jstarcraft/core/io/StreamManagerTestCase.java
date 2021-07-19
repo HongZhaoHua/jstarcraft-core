6b8855ed9f1049f95f30aa1e76c6b578dacc21e9
Birdy <Birdy@LAPTOP-R44M770M>
2021-07-19 22:03:54 +0800

    StreamManager单元测试.

---
+++ jstarcraft-core-io/src/test/java/com/jstarcraft/core/io/StreamManagerTestCase.java
@@ -0,0 +1,41 @@
+package com.jstarcraft.core.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jstarcraft.core.utility.StringUtility;
+import com.nimbusds.jose.util.IOUtils;
+
+public abstract class StreamManagerTestCase {
+
+    protected abstract StreamManager getStreamManager();
+
+    @Test
+    public void testHaveResource() throws Exception {
+        StreamManager manager = getStreamManager();
+        String path = "path";
+
+        {
+            Assert.assertFalse(manager.haveResource(path));
+            InputStream stream = manager.retrieveResource(path);
+            Assert.assertNull(stream);
+        }
+
+        try (InputStream stream = new ByteArrayInputStream(path.getBytes(StringUtility.CHARSET))) {
+            manager.saveResource(path, stream);
+            Assert.assertTrue(manager.haveResource(path));
+        }
+
+        try (InputStream stream = manager.retrieveResource(path)) {
+            String content = IOUtils.readInputStreamToString(stream, StringUtility.CHARSET);
+            Assert.assertEquals(path, content);
+        }
+
+        manager.waiveResource(path);
+        Assert.assertFalse(manager.haveResource(path));
+    }
+
+}
