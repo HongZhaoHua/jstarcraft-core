58d9aefa4437541c517884a7610f15dd4f25f7d7
Birdy <Birdy@LAPTOP-R44M770M>
2022-08-01 19:29:08 +0800

    重构路由策略.

--- jstarcraft-core-monitor/src/test/java/com/jstarcraft/core/monitor/route/database/RouteDataSourceTestCase.java
+++ jstarcraft-core-monitor/src/test/java/com/jstarcraft/core/monitor/route/database/RouteDataSourceTestCase.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.jstarcraft.core.monitor.route.NestRouteStrategy;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class RouteDataSourceTestCase {
